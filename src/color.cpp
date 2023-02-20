2fd136048084a467ffe508c662ced04fdba16956
doferreira2 <dorianferreira2@gmail.com>
2023-02-20 23:10:03 +0100

    Ajoute de plsusieur coleur repartie automatqiuement

--- src/color.cpp
+++ src/color.cpp
@@ -4,17 +4,16 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
 
-#define N_Class 10
+#define N_Class 5
 
-
-struct color{
-	float R
-	float V
-	float B
-}
-
-
+typedef struct s_color
+{
+	float R;
+	float V;
+	float B;
+} color;
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
 typedef CGAL::Polyhedron_3<Kernel> Polyhedron;
@@ -23,9 +22,18 @@ typedef Polyhedron::Facet_const_iterator Facet_iterator;
 typedef Polyhedron::Vertex_const_iterator Vertex_iterator;
 typedef Polyhedron::Halfedge_const_iterator Halfedge_iterator;
 typedef Polyhedron::Halfedge_around_facet_const_circulator Halfedge_facet_circulator;
+typedef Polyhedron::Facet_handle Facet_handle;
 
 typedef std::map<Polyhedron::Facet_const_handle, double> Facet_double_map;
 typedef std::map<Polyhedron::Facet_const_handle, int> Facet_int_map;
+
+// Define the color palette
+color colorPalette[N_Class] = { { 1.0, 0.0, 0.0 }, // red
+                               { 1.0, 0.5, 0.0 }, // orange
+                               { 1.0, 1.0, 0.0 }, // yellow
+                               { 0.0, 1.0, 0.0 }, // green
+                               { 0.0, 0.0, 1.0 }  // blue
+                             };
 
 /// @brief map all the values from [min, max] to [0, 1]
 /// @param facetMap non-const reference to the map (it is an in/out parameter)
@@ -50,46 +58,57 @@ void normalizeMap(Facet_double_map &facetMap)
 	for (auto &elem : facetMap)
 	{
 		elem.second -= minValue;
-		elem.second /= (maxValue-minValue);
+		elem.second /= (maxValue - minValue);
 	}
 }
 
-Facet_int_map simpleThershold(Facet_double_map &facetMap){
+Facet_int_map simpleThershold(Facet_double_map &facetMap)
+{
 	double avgValue = 0;
-	int nb = 0, em=0; 
+	int nb = 0, em = 0;
 	Facet_iterator i = 0;
 	Facet_int_map PerimInt;
 
-	// look for min and max value in the map
-	for (const auto &elem : facetMap)
-	{
-		avgValue += elem.second;
-		nb++;
-	}
-	avgValue /= nb;
+	// Initialize the min and max perimeters
+	double maxValue = facetMap.begin()->second;
+	double minValue = facetMap.begin()->second;
 
 	for (const auto &elem : facetMap)
 	{
-		
-
-		if (elem.second > avgValue)
+		if (elem.second > maxValue)
 		{
-			
-			PerimInt[elem.first] = 1;
-			
-		} else {
-			PerimInt[elem.first] = 0;
+			maxValue = elem.second;
 		}
+		if (elem.second < minValue)
+		{
+			minValue = elem.second;
+		}
+	}
+	double perimeterThreshold = (minValue + maxValue) / 2.0;
+	float intervalSize = (maxValue - minValue) / N_Class;
+
+
+	for (const auto &elem : facetMap)
+	{
+		int intervalIndex = (elem.second - minValue) / intervalSize;
+
+		if (intervalIndex < 0) {
+			intervalIndex = 0;
+		}
+		if (intervalIndex >= N_Class) {
+			intervalIndex = N_Class - 1;
+		}
+
+		PerimInt[elem.first] = intervalIndex;
 	}
 	return PerimInt;
 }
-
 
 /// @brief Generate in a .off file a colored mesh according to a value map (green to red shades)
 /// @param mesh the input mesh
 /// @param facetMap map of values between 0 and 1 (see "normalize()") for each facet of mesh
 /// @param filePath path to the colored .off file to be generated
-void writeOFFfromValueMap(const Polyhedron& mesh, const Facet_double_map& facetMap, Facet_int_map& facetIntM , std::string filePath)
+void writeOFFfromValueMap(const Polyhedron &mesh, const Facet_double_map &facetMap, Facet_int_map &facetIntM, std::string filePath)
 {
 	std::ofstream in_myfile;
 	in_myfile.open(filePath);
@@ -97,9 +116,9 @@ void writeOFFfromValueMap(const Polyhedron& mesh, const Facet_double_map& facetM
 	CGAL::set_ascii_mode(in_myfile);
 
 	in_myfile << "COFF" << std::endl // "COFF" makes the file support color informations
-			  << mesh.size_of_vertices() << ' ' 
-			  << mesh.size_of_facets() << " 0" << std::endl; 
-			  // nb of vertices, faces and edges (the latter is optional, thus 0)
+			  << mesh.size_of_vertices() << ' '
+			  << mesh.size_of_facets() << " 0" << std::endl;
+	// nb of vertices, faces and edges (the latter is optional, thus 0)
 
 	std::copy(mesh.points_begin(), mesh.points_end(),
 			  std::ostream_iterator<Kernel::Point_3>(in_myfile, "\n"));
@@ -117,11 +136,11 @@ void writeOFFfromValueMap(const Polyhedron& mesh, const Facet_double_map& facetM
 
 		} while (++j != i->facet_begin());
 
-		in_myfile << std::setprecision(5) << std::fixed; //set the format of floats to X.XXXXX
+		in_myfile << std::setprecision(5) << std::fixed; // set the format of floats to X.XXXXX
 
-		auto redValue = 1.0 -facetIntM.at(i); // low values will be closer to red
-		auto greenValue = 0.0 ; // high values will be closer to green
-		auto blueValue = 0.0 + facetIntM.at(i);
+		auto redValue = colorPalette[facetIntM.at(i)].R; // low values will be closer to red
+		auto greenValue = colorPalette[facetIntM.at(i)].V; 				   // high values will be closer to green
+		auto blueValue = colorPalette[facetIntM.at(i)].B;
 
 		in_myfile << " " << redValue << " " << greenValue << " " << blueValue;
 
@@ -160,9 +179,9 @@ Facet_double_map computeAreaMap(const Polyhedron &mesh)
 
 	for (Facet_iterator i = mesh.facets_begin(); i != mesh.facets_end(); ++i)
 	{
-	
+
 		Halfedge_facet_circulator j = i->facet_begin();
-		
+
 		Polyhedron::Vertex_const_handle firstVertex = j->vertex();
 
 		double current_area = 0;
@@ -171,7 +190,7 @@ Facet_double_map computeAreaMap(const Polyhedron &mesh)
 		// (illustration: http://mathbitsnotebook.com/JuniorMath/Polygons/polygons3g.jpg)
 		do
 		{
-			current_area += CGAL::squared_area(firstVertex->point(), j->vertex()->point(), j->opposite()->vertex()->point()); 
+			current_area += CGAL::squared_area(firstVertex->point(), j->vertex()->point(), j->opposite()->vertex()->point());
 		} while (++j != i->facet_begin());
 
 		std::cout << "area(" << std::distance(mesh.facets_begin(), i) << ")=" << current_area << std::endl;
@@ -200,16 +219,15 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	color t_color[N_class];
-
 	auto mapPerim = computePerimMap(mesh);
 
 	normalizeMap(mapPerim);
+
 	std::cout << "test1" << std::endl;
 	auto PerimInt = simpleThershold(mapPerim);
 	std::cout << "test2" << std::endl;
 
-	writeOFFfromValueMap(mesh, mapPerim,PerimInt, argc>=3?argv[2]:"result.off");
+	writeOFFfromValueMap(mesh, mapPerim, PerimInt, argc >= 3 ? argv[2] : "result.off");
 
 	return 0;
 }
